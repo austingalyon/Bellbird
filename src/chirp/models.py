38ebf116f06d9b91a0164cf7a19e550741242a04
Austin Galyon <austin.galyon@maxxpotential.com>
2019-11-20 14:23:07 -0500

    remove incomplete upvote code

--- src/chirp/models.py
+++ src/chirp/models.py
@@ -5,8 +5,3 @@ class Chirp(models.Model):
   text = models.CharField(max_length=140)
   upvotes = models.IntegerField(default=0)
   created_at = models.TimeField(auto_now_add=True)
-
-  def upvote(self):
-    upvotes = self.upvotes + 1
-    
-    return
